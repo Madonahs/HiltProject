248fe265de80cd203986dbe97f868ae3ae78de21
madonahs <syombuamadona@gmail.com>
2021-01-13 19:00:41 -0600

    test

--- app/src/main/java/com/madonasyombua/learninghilt/MainActivity.kt
+++ app/src/main/java/com/madonasyombua/learninghilt/MainActivity.kt
@@ -2,10 +2,16 @@ package com.madonasyombua.learninghilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        //navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/madonasyombua/learninghilt/StudentListsViewModel.kt
@@ -0,0 +1,7 @@
+package com.madonasyombua.learninghilt
+
+import androidx.lifecycle.ViewModel
+
+class StudentListsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
